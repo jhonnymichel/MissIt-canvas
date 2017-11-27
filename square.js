458b6c45b7492b1b04cb057278beddb50985640a
Jhonny Michel <jhonnymichel@evolux.net.br>
2017-11-27 19:08:32 -0300

    refactoring X and Y properties

--- square.js
+++ square.js
@@ -6,9 +6,34 @@ class Square {
   constructor(game, color='#00FF00') {
     this._game = game;
     this._area = game.area;
+    this._color = color;
+    this.createAxisProperties();
     this.x = 0;
     this.y = 0;
-    this._color = color;
+  }
+
+  createAxisProperties() {
+    const propertiesToCreate = [
+      {name: 'x', side: 'width'},
+      {name: 'y', side: 'height'}
+    ];
+    propertiesToCreate.forEach(axis => {
+      Object.defineProperty(this, axis.name, {
+        get() {
+          return this[`_${axis.name}`];
+        },
+        set(position) {
+          const maxPositionLimit = this._area[axis.name] + this._area[axis.side] - Square.SIZE;
+          const minPositionLimit = this._area[axis.name];
+          if (position > maxPositionLimit) {
+            position = maxPositionLimit;
+          } else if (position < minPositionLimit) {
+            position = minPositionLimit;
+          }
+          this[`_${axis.name}`] = position;
+        }
+      });
+    });
   }
 
   update(ctx) {
@@ -16,32 +41,6 @@ class Square {
     ctx.rect(this.x, this.y, Square.SIZE, Square.SIZE);
     ctx.fillStyle = this._color;
     ctx.fill();
-  }
-
-  get x() {
-    return this._x;
-  }
-
-  set x(x) {
-    if (x > this._area.x + this._area.width - Square.SIZE) {
-      x = this._area.x + this._area.width - Square.SIZE;
-    } else if (x < this._area.x) {
-      x = this._area.x;
-    }
-    this._x = x;
-  }
-
-  get y() {
-    return this._y;
-  }
-
-  set y(y) {
-    if (y > this._area.y + this._area.height - Square.SIZE) {
-      y = this._area.y + this._area.height - Square.SIZE;
-    } else if (y < this._area.y) {
-      y = this._area.y;
-    }
-    this._y = y;
   }
 }
 
