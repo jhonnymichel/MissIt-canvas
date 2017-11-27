671b5aa68008fbed6a7e4092abf45946bc1cf10c
Jhonny Michel <jhonnymichel@evolux.net.br>
2017-11-27 17:48:03 -0300

    adding new constants to game class

---
+++ README.md
@@ -0,0 +1,5 @@
+Run NPM Install, then run NPM Start.
+
+then http://localhost:8080.
+
+You better have the latest version of Google Chrome to run this cuz I ain't transpiling the modules to ES5, sorry. let's keep it simple, pals.
--- src/game.js
+++ src/game.js
@@ -5,11 +5,15 @@ class Game {
   }
 
   static get INITIAL_SPEED() {
-    
+    return 5;
+  }
+
+  static get SPEED_INCREMENTER() {
+    return 0.5;
   }
 
   constructor(area={x: 0, y: 0, width: 200, height: 200}) {
-    this.speed = 10;
+    this._speed = Game.INITIAL_SPEED;
     this.score = 0;
     this.area = area;
     window.addEventListener('incrementscore', this.incrementScore.bind(this))
@@ -36,11 +40,11 @@ class Game {
     return this._speed;
   }
 
-  set speed(incrementer) {
+  set speed(value) {
     if (this._speed >= 30) return;
     window.dispatchEvent(new CustomEvent('incrementscore'));
     window.dispatchEvent(new CustomEvent('speedchanged'));
-    this._speed = incrementer;
+    this._speed += Game.SPEED_INCREMENTER;
   }
 }
 
