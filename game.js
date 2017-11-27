13d09ac63f0f2ab735e7d2c0b3009a13c3123a48
Jhonny Michel <jhonnymichel@evolux.net.br>
2017-11-27 17:35:42 -0300

    tweaking speed and score

--- game.js
+++ game.js
@@ -1,11 +1,22 @@
 class Game {
+
+  static get SPEED_INTERVAL() {
+    return 500;
+  }
+
+  static get INITIAL_SPEED() {
+    
+  }
+
   constructor(area={x: 0, y: 0, width: 200, height: 200}) {
     this.speed = 10;
+    this.score = 0;
     this.area = area;
+    window.addEventListener('incrementscore', this.incrementScore.bind(this))
   }
 
   start() {
-    this._intervalId = setInterval(() => this.speed++, 1000 * 10);
+    this._intervalId = setInterval(() => this.speed++, Game.SPEED_INTERVAL * 10);
   }
 
   pause() {
@@ -17,13 +28,18 @@ class Game {
     this.speed = 1;
   }
 
+  incrementScore() {
+    this.score++;
+  }
+
   get speed() {
     return this._speed;
   }
 
   set speed(incrementer) {
     if (this._speed >= 30) return;
-    window.dispatchEvent(new CustomEvent("speedchanged"));
+    window.dispatchEvent(new CustomEvent('incrementscore'));
+    window.dispatchEvent(new CustomEvent('speedchanged'));
     this._speed = incrementer;
   }
 }
